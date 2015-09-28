8107a720b1b475b77161510fcb0378204f9cdcf5
Daniel Andrews <Danielandrewsre@gmail.com>
2015-09-27 23:47:25 -0700

    flappy complete?

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 force;
+	public static float speed = 5;
+	public KeyCode jump;
+
+	Rigidbody2D player;
+
+	bool flap = false;
+	bool isAlive = true;
+
+	void Start(){
+		player = GetComponent<Rigidbody2D> ();
+		player.AddForce (force, ForceMode2D.Impulse);
+		player.AddForce (Vector2.right * speed, ForceMode2D.Impulse);
+	}
+
+
+	void Update(){
+
+		if (!isAlive)
+			return;
+
+		if (Input.GetKeyDown (jump)) {
+			flap = true;
+		}
+
+	}
+
+	void FixedUpdate(){
+
+		if (!isAlive)
+			return;
+
+		if (flap) {
+			player.AddForce(force, ForceMode2D.Impulse);
+			flap = false;
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		isAlive = false;
+	}
+}
+
---
+++ Assets/Scripts/SlowBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowBackground : MonoBehaviour {
+
+	Rigidbody2D player;
+
+	void Start(){
+		GameObject player_ob = GameObject.FindGameObjectWithTag("Player");
+		if (player_ob == null) {
+			Debug.Log ("No player found");
+			return;
+		}
+
+		player = player_ob.GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		Vector3 pos = transform.position;
+		pos.x += player.velocity.x * Time.deltaTime * 0.9f;
+		transform.position = pos;
+	}
+}
