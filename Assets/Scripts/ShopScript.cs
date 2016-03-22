bd15e29adb3e78a1709b03e7336decd5e233ec53
DanielAndrews43 <danielandrewsre@gmail.com>
2016-03-22 01:37:57 -0700

    shop snap
    working shop snap with skin instantiate

--- Assets/Scripts/ShopScript.cs
+++ Assets/Scripts/ShopScript.cs
@@ -7,13 +7,13 @@ public class ShopScript : MonoBehaviour {
 	public GameObject[] skinPrefabs;
 	GameObject[] skins;
 	public GameObject skinHolder;
-	ScrollRect scroller;
 	public GameObject scrollBar;
+	public RectTransform center;
+	public RectTransform panel;
 
-	Vector3 scrollBarCenter;
 	float scrollBarWidthHalf;
 
-	bool isBeingScrolled;
+	bool dragging;
 
 	public float speed;
 
@@ -21,17 +21,66 @@ public class ShopScript : MonoBehaviour {
 	public int maxSize;
 	public float distance;
 
+	bool firstTime = true;
+	float skinDistance;
+
+	int index;
+	float min;
+
 	void Start () {
 
 		skins = new GameObject[skinPrefabs.Length];
-
-		scroller = scrollBar.GetComponent<ScrollRect> ();
-
-		scrollBarCenter = scrollBar.transform.position;
 		scrollBarWidthHalf = scrollBar.GetComponent<RectTransform> ().rect.x * -1;
 
+		MakeSkins ();
+
+		skinDistance = Mathf.Abs (skins [1].GetComponent<RectTransform> ().anchoredPosition.x - skins [0].GetComponent<RectTransform> ().anchoredPosition.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ResizeSkins ();
+
+		if (!dragging) {
+			SnapToNearest ();
+			firstTime = false;
+		}
+
+	}
+
+	void SnapToNearest(){
+		if (firstTime) {
+			index = 0;
+			min = 100000;
+			for (int i = 0; i < skins.Length; i++) {
+				if (Mathf.Abs (skins [i].transform.position.x - center.transform.position.x) < min) {
+					min = Mathf.Abs (skins [i].transform.position.x - center.transform.position.x);
+					index = i;
+				}
+			}
+		}
+
+		float position = index * -skinDistance;
+
+		float newX = Mathf.Lerp (panel.anchoredPosition.x, position, Time.deltaTime * 10f);
+		Vector2 newPos = new Vector2 (newX, panel.anchoredPosition.y);
+
+		panel.anchoredPosition = newPos;
+	}
+
+	public void BeginDrag(){
+		dragging = true;
+	}
+
+	public void EndDrag(){
+		dragging = false;
+		firstTime = true;
+	}
+
+	void MakeSkins(){
 		for (int i = 0; i < skinPrefabs.Length; i++) {
-			Vector3 GOPosition = new Vector3 (scrollBarCenter.x + (scrollBarWidthHalf * distance * i), scrollBarCenter.y);
+			Vector3 GOPosition = new Vector3 (center.transform.position.x + (scrollBarWidthHalf * distance * i), center.transform.position.y);
 
 			GameObject GO = (GameObject)Instantiate (skinPrefabs [i], GOPosition, Quaternion.identity);
 			GO.transform.SetParent (skinHolder.transform);
@@ -39,48 +88,17 @@ public class ShopScript : MonoBehaviour {
 			skins [i] = GO;
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if (scroller.velocity.Equals(Vector2.zero)) {
-			isBeingScrolled = false;
-		} else {
-			isBeingScrolled = true;
-		}
 
+	void ResizeSkins(){
 		for (int i = 0; i < skins.Length; i++) {
-			float difference = Mathf.Abs (scrollBarCenter.x - skins [i].transform.position.x);
+			float difference = Mathf.Abs (center.transform.position.x - skins [i].transform.position.x);
 			float percentage = difference / scrollBarWidthHalf;
-			float proportion = maxSize - (1 * Mathf.Pow ((percentage), exponent));
+			float proportion = maxSize - Mathf.Pow ((percentage), exponent);
 
 			if (proportion < 1) {
 				proportion = 1;
 			}
-			skins [i].transform.localScale = new Vector3 (proportion, proportion, 1);
-		}
-
-		if (!isBeingScrolled) {
-			SnapToNearest ();
-		}
-
-	}
-
-	void SnapToNearest(){
-		float min = Mathf.Abs (scrollBarCenter.x - skins [0].transform.position.x);
-		int index = 0;
-
-		for (int i = 0; i < skins.Length; i++) {
-			if (Mathf.Abs (scrollBarCenter.x - skins [i].transform.position.x) < min) {
-				min = Mathf.Abs (scrollBarCenter.x - skins [i].transform.position.x);
-				index = i;
-			}
-		}
-
-		//move scrollbar until position of GO at index i = center;
-		if(scrollBarCenter.x - skins [index].transform.position.x < 0){ // to the right
-			float step = speed * Time.deltaTime;
-
+			skins [i].transform.localScale = new Vector3 (proportion, proportion);
 		}
 	}
 }
